e7d5b1eca422d8228b46c85bf2d938e173508c0d
Eduardo Czpla <edu.czpla@gmail.com>
2022-05-03 12:09:02 -0300

    feature winston logs

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 const express = require('express');
 const app = express();
 
+const logger = require('../src/utils/logger');
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
+    logger.log('info', { body: req.body, method: req.method, useragent: req.headers['user-agent'], headers: req.headers });
     res.send('<h1>Ola!</h1>');
 });
 
 app.get('/geterror', (req, res) => {
-    res.send(100/0);
+    logger.log('info', { body: req.body, method: req.method, useragent: req.headers['user-agent'], headers: req.headers });
+    res.status(500).send('get error');
 });
 
 app.post('/login', (req, res) => {
+    logger.log('info', { body: req.body, method: req.method, useragent: req.headers['user-agent'], headers: req.headers });
     res.send(req.body);
 });
 
 app.post('/posterror', (req, res) => {
-    res.send(100/0);
+    logger.log('info', { body: req.body, method: req.method, useragent: req.headers['user-agent'], headers: req.headers });
+    res.status(500).send('error posterror');
 });
 
 app.listen(3000, () => {
